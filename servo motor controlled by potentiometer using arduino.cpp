20363c417683f1814cbef81bfeb73682701b49e4
FranklynThP <130224393+FranklynThP@users.noreply.github.com>
2023-05-11 13:21:07 -0300

    Servo motor controlled by potentiometer using arduino.cpp

--- servo motor controlled by potentiometer using arduino.cpp
+++ servo motor controlled by potentiometer using arduino.cpp
@@ -1,26 +1,40 @@
 #include <Servo.h>
 
-#define pinServo 8
-#define pinPotenciometro A0
-
-
 Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
 
+int servo1_pin = 9;
+int servo2_pin = 10;
+int servo3_pin = 11;
+int servo4_pin = 12;
+
+int pot1_pin = A0;
+int pot2_pin = A1;
+int pot3_pin = A2;
+int pot4_pin = A3;
 
 void setup() {
-  servo1.attach(pinServo);
-  Serial.begin(9600);
+  servo1.attach(servo1_pin);
+  servo2.attach(servo2_pin);
+  servo3.attach(servo3_pin);
+  servo4.attach(servo4_pin);
 }
-
 void loop() {
-  int lido = analogRead(pinPotenciometro);
-  byte angulo = map(lido, 0, 1023, 0, 179);
+  int val1 = analogRead(pot1_pin);
+  int val2 = analogRead(pot2_pin);
+  int val3 = analogRead(pot3_pin);
+  int val4 = analogRead(pot4_pin);
 
-  servo1.write(angulo);
+  int servo1_pos = map(val1, 0, 1023, 0, 179);
+  int servo2_pos = map(val2, 0, 1023, 0, 179);
+  int servo3_pos = map(val3, 0, 1023, 0, 179);
+  int servo4_pos = map(val4, 0, 1023, 0, 179);
 
-  Serial.print("A0:");
-  Serial.print(lido);
-  Serial.print("angulo: ");
-  Serial.println(angulo);
-
+  servo1.write(servo1_pos);
+  servo2.write(servo2_pos);
+  servo3.write(servo3_pos);
+  servo4.write(servo4_pos);
+  
 }
