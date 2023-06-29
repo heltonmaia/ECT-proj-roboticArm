c491c76a56772b2c7561e89ae9fd344c3df8ca71
Kennymar Oliveira <87088060+KennymarOliveira@users.noreply.github.com>
2023-06-29 13:50:29 -0300

    Update pwm_driver_with_variable_speed.ino

--- pwm_driver_with_variable_speed.ino
+++ pwm_driver_with_variable_speed.ino
@@ -8,11 +8,13 @@
 
 // Cria os objetos (driver + servos)
 Adafruit_PWMServoDriver driver = Adafruit_PWMServoDriver();
-VarSpeedServo servo1, servo2;
+VarSpeedServo garra, haste1, haste2, base;
 
 // Indice dos servos no driver (valores de 0 a 15)
-unsigned int servo1_pin = 12;
-unsigned int servo2_pin = 15;
+unsigned int garra_pin = 12;
+unsigned int haste1_pin = 13;
+unsigned int haste2_pin = 14;
+unsigned int base_pin = 15;
 
 // Função para mover a posição de um servo informando o angulo em graus
 void mover(VarSpeedServo& servo, unsigned int velocidade, unsigned int angulo){
@@ -35,16 +37,18 @@ void setup() {
   driver.setPWMFreq(60);
 
   // Associa os servos aos seus respectivos indices no PWM Driver
-  servo1.attach(servo1_pin);
-  servo2.attach(servo2_pin);
+  garra.attach(garra_pin);
+  haste1.attach(haste1_pin);
+  haste2.attach(haste2_pin);
+  base.attach(base_pin);
 }
 
 void loop() {
   
-  // Move o servo 1 para angulo 90° com velocidade em 30%
-  mover(servo1, 90, 30);
-
-  // Move o servo 2 para angulo de 120° com velocidade de 45%
-  mover(servo2, 120, 45);
+  // Move os servos
+  mover(garra, 90, 30);
+  mover(haste1, 90, 30);
+  mover(haste2, 90, 30);
+  mover(base, 90, 30);
 
 }
