d69c9e3859e9b8ed66625d9a7b1de57df534e07f
Helton Maia <heltonmaia@users.noreply.github.com>
2023-11-11 12:42:18 -0300

    Update voiceControl.py

--- voiceControl.py
+++ voiceControl.py
@@ -12,7 +12,7 @@ counter = 0
 # Start listening for speech
 with mic as source:
     print("Listening...")
-    audio = r.record(source, duration=5)  # Record for 3 seconds
+    audio = r.record(source, duration=5)  # Record for 5 seconds
     print("Recording...")
 
 # Try to recognize the speech
