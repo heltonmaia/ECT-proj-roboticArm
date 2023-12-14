342f40d48a184ac945e8289b58a67be4a972c8b7
Kennymar Oliveira <87088060+KennymarOliveira@users.noreply.github.com>
2023-12-14 13:11:46 -0300

    Atualizar o serial port command receiver v3.cpp

--- tests/new/serial port command receiver v3.cpp
+++ tests/new/serial port command receiver v3.cpp
@@ -1,33 +1,35 @@
 #include <VarSpeedServo.h>
 
-#define g_speed 30
-#define b_speed 30 
-#define h_speed 15
-#define haste1_pin 3
-#define haste2_pin 4
-#define base_pin 5
-#define garra_pin 2
+#define gripper_speed 30
+#define rotating_base_speed 30 
+#define arms_speed 15
+#define arm1_pin 3
+#define arm2_pin 4
+#define rotating_base_pin 5
+#define gripper_pin 2
 
-VarSpeedServo garra;
-VarSpeedServo base;
-VarSpeedServo haste1;
-VarSpeedServo haste2;
-int angulo_base = 0;
-int angulo_garra = 0;
-int angulo_haste1 = 0;
-int angulo_haste2 = 0;
+VarSpeedServo gripper;
+VarSpeedServo rotating_base;
+VarSpeedServo arm1;
+VarSpeedServo arm2;
+int rotating_base_angle = 0;
+int gripper_angle = 0;
+int arm1_angle = 0;
+int arm2_angle = 0;
 
 void setup() {
-  base.attach(base_pin);
-  garra.attach(garra_pin);
-  haste1.attach(haste1_pin);
-  haste2.attach(haste2_pin);
+  rotating_base.attach(rotating_base_pin);
+  gripper.attach(gripper_pin);
+  arm1.attach(arm1_pin);
+  arm2.attach(arm2_pin);
 
   Serial.begin(9600);
 }
 
 void loop() {
+
     if(Serial.available() >= 4){
+
     angulo_base = Serial.read();
     angulo_garra = Serial.read();
     angulo_haste1 = Serial.read();
