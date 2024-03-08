c092c0265ad5fa4aa004bf6e6e26314514f3cd74
Thiago Vinicius <127901647+thiagoclopes@users.noreply.github.com>
2024-03-08 13:16:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -29,16 +29,11 @@ prev_frame_time = 0
 new_frame_time = 0
 pixel_threshold = 5
 
-def releaseConnections(cap):
-    # Libera o acesso à câmera
-    if cap.isOpened():
-        cap.release()
-    cv2.destroyAllWindows()
+
 
 while cap.isOpened():
     read, frame = cap.read()
     frame = cv2.flip(frame, 1)
-
     if read:
         results = model(source=frame, conf=0.6)
 
@@ -65,10 +60,9 @@ while cap.isOpened():
 
                 in_range = is_in_range(m_coord_x, m_coord_y)
 
-                cv2.putText(frame, f'Area {i + 1}: {area:.2f}', (int(box.xyxy[0][0]), int(box.xyxy[0][3])),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+                text_area(frame, i, area, int(box.xyxy[0][0]), int(box.xyxy[0][3]))
+                draw_center(frame, m_coord_x, m_coord_y)
 
-                cv2.circle(frame, (m_coord_x, m_coord_y), 3, (0, 255, 0), 2)
 
                 direction = calculate_direction(pixel_threshold, m_coord_x, m_coord_y, prev_m_coord_x, prev_m_coord_y)
 
@@ -94,20 +88,7 @@ while cap.isOpened():
             in_range = False
             print('No hand detection\n')
 
-        # Configurações da janela principal
-        window_size = (920, 500)
-        window = np.zeros((window_size[1], window_size[0], 3), dtype=np.uint8)
-        bg_color = (35, 15, 0)
-        window[:, :] = bg_color
-
-        # Posição e tamanho do feed de vídeo na janela
-        video_position = (270, 10)
-        video_size = (640, 480)
-
-        # Desenha o feed de vídeo na posição desejada na janela e suas informações
-        annotated_frame = results[0].plot(boxes=False)
-        window[video_position[1]:video_position[1] + video_size[1],
-        video_position[0]:video_position[0] + video_size[0]] = annotated_frame
+        window = window_config(results[0].plot(boxes=False))
         draw_info(m_coord_x, m_coord_y, window, cap_device, COM, detected, in_range, state, direction, fps)
         cv2.imshow('Robotic Arm Control', window)
 
