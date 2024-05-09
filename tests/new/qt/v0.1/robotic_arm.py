2181b54f953909a5afea9f2ea4728f6b96b40c60
Kennymar Oliveira <87088060+KennymarOliveira@users.noreply.github.com>
2024-05-09 14:19:50 -0700

    Add files via upload

---
+++ tests/new/qt/v0.1/robotic_arm.py
@@ -0,0 +1,45 @@
+import struct
+import numpy as np
+from typing import Tuple, Any
+from numpy import ndarray, dtype, floating, float_
+from numpy._typing import _64Bit
+
+def control_arm(ser, rotating_base_angle: int, gripper_angle: int, arm1_angle: int, arm2_angle: int):
+    ser.write(struct.pack('BBBB', rotating_base_angle, gripper_angle, arm1_angle, arm2_angle))
+
+def control_arm(rotating_base_angle: int, gripper_angle: int, arm1_angle: int, arm2_angle: int):
+    print(rotating_base_angle, gripper_angle, arm1_angle, arm2_angle)
+
+def calculate_rotating_base_angle(m_coord_x: float) -> int:
+    rotating_base_angle = map(m_coord_x, 110, 530, 0, 180)
+    return int(rotating_base_angle)
+
+
+def calculate_angle_hastes(m_coord_y: float) -> Tuple[int, int]:
+    arm2_angle = map(m_coord_y, 80, 400, 95, 180)
+    aux_h1 = (180 * 95) // arm2_angle
+    return int(aux_h1), int(arm2_angle)
+
+
+def calculate_gripper_angle(garra_state: bool) -> Tuple[int, str]:
+    if garra_state == 0:
+        return 10, "Closed"
+    elif garra_state == 1:
+        return 120, "Open"
+
+
+def is_moving_axis_x(pixel_threshold: int, prev_m_coord_x: float, m_coord_x: float) -> bool:
+    if abs(prev_m_coord_x - m_coord_x) > pixel_threshold:
+        return True
+    return False
+
+
+def is_moving_axis_y(pixel_threshold: int, prev_m_coord_y: float, m_coord_y: float) -> bool:
+    if abs(prev_m_coord_y - m_coord_y) > pixel_threshold:
+        return True
+    return False
+
+
+def map(value: float, min_val: float, max_val: float, min_target: float, max_target: float) -> ndarray[
+    Any, dtype[floating[_64Bit] | float_]]:
+    return np.interp(value, [min_val, max_val], [min_target, max_target])
